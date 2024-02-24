7c5825b229136a316a151e5abc3361005cbf4108
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-02-25 04:33:27 +0600

    added some card and silder part and jquery plugin

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// ====== jQuery operations ======
+$(function(){
+    // ==== slick slider operation ====
+    $('.suggest_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.suggest_prevArrow',
+        nextArrow: '.suggest_nextArrow',
+        
+
+    });
+
+
+    // ==== slick slider operation ====
+
+
+
+
+
+
+})
+// ====== jQuery operations ======
