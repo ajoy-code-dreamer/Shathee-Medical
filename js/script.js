bb153d8437f365277e039fb6ce40ea05d00ebbb7
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-02-25 21:22:08 +0600

    added some slider section

--- js/script.js
+++ js/script.js
@@ -6,8 +6,27 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '.suggest_prevArrow',
         nextArrow: '.suggest_nextArrow',
-        
-
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+    });
+    $('.follow_dietSlider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '.flowDiet_prevArrow',
+        nextArrow: '.flowDiet_nextArrow',
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+    });
+    $('.related_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '.related_prevArrow',
+        nextArrow: '.related_nextArrow',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
     });
 
 
