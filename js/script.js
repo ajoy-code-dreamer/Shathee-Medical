2405741274131c390fdad557162c0fa4aae8ff46
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-02-26 21:37:50 +0600

    complete all page  responsive design

--- js/script.js
+++ js/script.js
@@ -9,6 +9,31 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 
     $('.follow_dietSlider').slick({
@@ -19,6 +44,31 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 1000,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
 
     $('.related_slider').slick({
@@ -29,6 +79,33 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            }
+          ]
     });
 
     $('.toDo_slider1').slick({
@@ -39,6 +116,33 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            }
+          ]
     });
     $('.toDo_slider2').slick({
         slidesToShow: 3,
@@ -48,6 +152,34 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows:false
+              }
+            }
+          ]
     });
 
 
