892da79f4275f0643b72a23bf46bc59a1a038802
ajoy-code-dreamer <ajoy.code.dreamer@gmail.com>
2024-02-27 02:49:30 +0600

    fix some bugs

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,21 @@
-const bpPage = document.querySelector(".bp_cardWrapper.bp_grpah_wrapper");
+const bpPage = document.querySelector(".bpPageSrc");
 
       bpPage.addEventListener("click", () => {
         window.location.href = "../pages/bloodPressure.html";
       });
-const glucosePage = document.querySelector("bp_cardWrapper.sugar_cardWrapper");
+
+const glucosePage = document.querySelector(".glucosePageSrc");
 
 glucosePage.addEventListener("click", () => {
         window.location.href = "../pages/bloodSugar.html";
       });
 
-const physicalPage = document.querySelector("bp_cardWrapper.sugar_cardWrapper.physical_cardWrapper");
+
+const physicalPage = document.querySelector(".physicalPageSrc");
 
 physicalPage.addEventListener("click", () => {
         window.location.href = "../pages/physicalMental.html";
       });
+
+
+      console.log(physicalPage)
